58fd4cc239bb766020a2a26cb65ae3b21b666839
Sunil <sunilbujji373@gmail.com>
2025-01-06 16:35:21 +0530

    shell script

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MONGDB_HOST=sunil-devops.shop
+MONGDB_HOST=mongodb.sunil-devops.shop
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
